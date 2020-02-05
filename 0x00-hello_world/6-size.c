2da3ff18cd8df86a510a3b6d3d59aaa1191149ef
daniloromero <1601@holbertonschool.com>
2020-02-05 22:45:33 +0000

    test 1

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char is: %lu.\n", sizeof(c));
+printf("Size of  an int is: %lu.\n", sizeof(int));
+printf("Size of an c is: %lu.\n", sizeof(c));
+return (0);
+}
