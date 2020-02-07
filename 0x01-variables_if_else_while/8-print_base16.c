2d4f96085c3e57c96d1098de32f0ffb47a73a465
daniloromero <1601@holbertonschool.com>
2020-02-07 20:59:32 +0000

    test 1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Hexadecinal 
+ *Description: Hexadecimal digits sample
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = '0'; l <= '9'; l++)
+		putchar(l);
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
