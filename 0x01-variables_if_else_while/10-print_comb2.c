da851a9c9a9c223f4b4f997d4ab5ae5e8293e213
daniloromero <1601@holbertonschool.com>
2020-02-07 22:49:00 +0000

    test 1

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main -  00...99
+ * Description: prints numbers 00...99
+ * Return: 0
+ */
+int main(void)
+{
+	int l;
+	int k;
+
+	for (l = '0'; l <= '9'; l++)
+	{
+		for (k = '0'; k <= '9'; k++)
+		{
+			putchar(l);
+			putchar(k);
+		if (l != '9' && k != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
+		}
+		}	
+	}
+	return (0);
+}
