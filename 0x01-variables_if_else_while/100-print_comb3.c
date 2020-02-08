1014db056d772fd3fe36c8707e3e551d80467dec
daniloromero <1601@holbertonschool.com>
2020-02-08 15:27:11 +0000

    test1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main -   Inventing is a combination
+ * Description:a program prints all possible different combinations of 2 digits
+ * Return: 0
+ */
+int main(void)
+{
+	int l;
+	int k;
+
+	for (l = '0'; l <= '8'; l++)
+	{
+		for (k = l++; k <= '9'; k++)
+		{
+			putchar(l);
+			putchar(k);
+			if (l == '8' && k == '9')
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
