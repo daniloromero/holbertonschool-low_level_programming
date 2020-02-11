2b707a41e7109f0a5cd11b15aa02a5a0e5a3eb23
daniloromero <1601@holbertonschool.com>
2020-02-11 20:16:51 +0000

    rechecked

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,15 +10,14 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-n = 1;
+return (1);
 }
 else if (n < 0)
 {
 _putchar('-');
-n = -1;
+return (-1);
 }
 else
 _putchar('0');
-n = 0;
 return (n);
 }
