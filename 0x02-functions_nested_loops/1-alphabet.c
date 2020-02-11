411b72dada25ed6b5beb5afdad80bfbf357c33fd
daniloromero <1601@holbertonschool.com>
2020-02-11 15:59:57 +0000

    test1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
 #include "holberton.h"
+/**
+ * main - print alphabet
+ *Description  prints the alphabet, in lowercase
+ lphabet, in lowercase* Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char abc;
+for (abc = 'a'; abc <= 'z'; abc++)
+{
+_putchar(abc);
+}
+_putchar('\n');
+
+}
