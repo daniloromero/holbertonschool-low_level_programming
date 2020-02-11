267a055440edf32b8571c38f08cdfe662a890add
daniloromero <1601@holbertonschool.com>
2020-02-11 15:36:13 +0000

    test 1

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - _putchar
+ *Description print word with putchar function
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+return (0);
+}
