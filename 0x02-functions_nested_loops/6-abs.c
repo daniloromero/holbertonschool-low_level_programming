46947fda65f7003493bfba01c55ea0bc896ca0b9
daniloromero <1601@holbertonschool.com>
2020-02-11 19:53:03 +0000

    tested file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,16 @@
 #include "holberton.h"
+/**
+ * _abs - print absolute number.
+ *@int: print absolute number
+ * Return: 0.
+ */
+int _abs(int ab)
+{
+if (ab >= 0)
+{
+_putchar(ab);
+}
+else
+_putchar(ab * -1);
+return (0);
+}
