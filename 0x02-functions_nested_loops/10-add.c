4c925fa7638561fa9b290a964676b616e405aeac
daniloromero <1601@holbertonschool.com>
2020-02-12 03:40:04 +0000

    tested file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,10 @@
 #include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ * @int:dds two integers and returns the result
+ * Return: Always 0.
+ */
+
+int add(int, int)
+{
+return (0);
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,11 @@
 #include "holberton.h"
+/**
+ * times_table - prints 9 times table
+ *@void:time tables
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	
+return (0);
+}
