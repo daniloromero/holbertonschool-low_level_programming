d27bf6c2c2f98078d03527af8d75212d42804205
daniloromero <1601@holbertonschool.com>
2020-02-18 22:53:18 +0000

    1st version tested file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * puts_half - prints half characters of a string
+ * Description:prints half  string
+ * @str:string to pick even characthers
+ */
+void puts_half(char *str)
+{
+	int index = 0;
+	int half;
+
+	while (str[index] != '\0')
+	{
+		index++;
+	}
+	if (index % 2 == 0)
+	{
+		half = index / 2;
+	}
+	else
+	{
+		half = (index - 1) / 2;
+	}
+	while (half <= index)
+	{
+		_putchar(*(str + half));
+		half++;
+	}
+	_putchar('\n');
+}
