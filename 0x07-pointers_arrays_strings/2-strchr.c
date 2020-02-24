10c04f1b9574bc0a71f96f34c4549153782e9e17
daniloromero <1601@holbertonschool.com>
2020-02-24 21:53:11 +0000

    tested file 1st version

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *_strchr -that locates a character in a string.
+ *@s:string to scan for charachter
+ *@c: fist occurrance of character to be returned
+ *Return:s
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] != c; index++)
+	{
+	}
+	if (s[index] == c)
+		s = (s + index);
+	return (s);
+}
