3ee60430b65be03240cee40149aeae1ecef8569d
daniloromero <1601@holbertonschool.com>
2020-02-26 20:57:21 +0000

    added retrun in comments

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,13 +4,13 @@
  *_pow_recursion -  returns the value of x raised to the power of y
  *@x: given number
  *@y: to the power of
- * Return: .
+ * Return:0 .
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	if (y == 0)
-		return(1);
+		return (1);
 	return (x * _pow_recursion(x, y - 1));
 }
