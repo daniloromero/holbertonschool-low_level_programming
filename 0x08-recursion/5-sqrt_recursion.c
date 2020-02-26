60134f23c3a78b722b60bda7164edd0c83bd8e85
daniloromero <1601@holbertonschool.com>
2020-02-26 23:08:53 +0000

    1st version tested file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _product - returns result z * z
+ * @z: multiplier
+ * @n:expected result
+ * Return: 0
+ */
+int _product(int z, int n)
+{
+	if (z == n || z > n)
+		return (-1);
+	if ((z * z) == n)
+		return (z);
+	else
+		z++;
+		return (_product(z, n));
+}
+
+
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number.
+ *@n: given number
+ * Return:0 .
+ */
+int _sqrt_recursion(int n)
+{
+	int z = 2;
+
+	if (n == 1)
+		return (1);
+	return (_product(z, n));
+}
