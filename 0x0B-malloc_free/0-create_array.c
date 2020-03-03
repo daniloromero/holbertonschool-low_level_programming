979019c27778d7e4aab33e35dc7800c8352b299c
daniloromero <1601@holbertonschool.com>
2020-03-03 19:37:37 +0000

    array validation added

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,10 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int index;
 
-	array = malloc(sizeof(char) * size);
 	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
 		return (NULL);
 	for (index = 0; index < size; index++)
 		array[index] = c;
