6cd2b468af0808072a5b4d3de86a587074e75e8a
daniloromero <1601@holbertonschool.com>
2020-03-03 18:27:39 +0000

    set all to 0x48

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	for (index = 0; index < size; index++)
-		array[0] = c;
+		array[index] = c;
 	return (array);
 
 }
