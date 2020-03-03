bb6915cd64359f676f0cd364094a1b97ce34ebad
daniloromero <1601@holbertonschool.com>
2020-03-03 17:50:09 +0000

    1st version tested in console

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: number of elements in array
+ * Return:0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
+
+	array = malloc(sizeof(char) * size);
+	if (size == 0)
+		return (NULL);
+	for (index = 0; index < size; index++)
+		array[0] = c;
+	return (array);
+
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup -  returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return:0
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	int index;
+	int size = 0;
+
+	if (str == '\0')
+		return (NULL);
+	while (size <= *str)
+	{
+		size++;
+	}
+	array = malloc(sizeof(char) * size);
+	for (index = 0; index < size; index++)
+		array[index] = str[index];
+	return (array);
+}
