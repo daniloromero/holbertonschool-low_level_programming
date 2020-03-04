c489c3fefde2c2060334a1a21246f0bdf940dd07
daniloromero <1601@holbertonschool.com>
2020-03-04 18:25:20 +0000

    swaped height and with in alloc function

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,15 +17,15 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = malloc(sizeof(int *) * width);
+	array = malloc(sizeof(int *) * height);
 	if (array == NULL)
 	{
 		free(array);
 		return (NULL);
 	}
-	for (i1 = 0; i1 < width; i1++)
+	for (i1 = 0; i1 < height; i1++)
 	{
-		array[i1] = malloc(sizeof(int) * height);
+		array[i1] = malloc(sizeof(int) * width);
 		if (array[i1] == NULL)
 		{
 			for (x = 0; x < i1; x++)
@@ -34,9 +34,9 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	for (i1 = 0; i1 < width; i1++)
+	for (i1 = 0; i1 < height; i1++)
 	{
-		for (i2 = 0; i2 < height; i2++)
+		for (i2 = 0; i2 < width; i2++)
 		{
 			array[i1][i2] = 0;
 		}
