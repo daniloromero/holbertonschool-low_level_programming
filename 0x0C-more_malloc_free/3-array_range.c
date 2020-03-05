439a7895374b1d8845801c5e5ec74b66c2972774
daniloromero <1601@holbertonschool.com>
2020-03-05 22:10:29 +0000

    added loop to fill memory

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,11 +9,15 @@
 int *array_range(int min, int max)
 {
 	int *array;
+	int i, i2;
 
 	if (min > max)
 		return (NULL);
-	array = malloc(sizeof(int) * (max - min + 1));
+	i = (max - min +1);
+	array = malloc(sizeof(int) * i);
 	if (array == NULL)
 		return (NULL);
+	for (i2 = 0; i2 < i; i2++)
+		array[i2] = i2;
 	return (array);
 }
