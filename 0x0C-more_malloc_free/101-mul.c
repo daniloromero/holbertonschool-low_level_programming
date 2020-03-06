0aa13981e9c7f802f1bf35a236797d690c988e47
daniloromero <1601@holbertonschool.com>
2020-03-06 02:00:52 +0000

    added stdio + stdlib

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies 2 integers
  * @argc: counter of arguments
@@ -10,15 +11,17 @@ int main(int argc, char *argv[])
 {
 	long int mul;
 	long int num1 = atoi(argv[1]);
-	long int num1 = atoi(argv[2]);
+	long int num2 = atoi(argv[2]);
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
-
+	}
 	if (argc)
+	{
 		mul = num1 * num2;
-			printf("%i\n", mul);
-			return (0);
+			printf("%li\n", mul);
+	}
+	return (0);
 }
