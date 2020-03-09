ac2782774674dd45618c87b80ad1b429d01b11bf
daniloromero <1601@holbertonschool.com>
2020-03-09 18:32:53 +0000

    1st version teste on console

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print file name using macro __FILE__
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
