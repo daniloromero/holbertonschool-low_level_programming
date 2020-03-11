4e24209709d6778941eb76d4b20757ae143a72d9
daniloromero <1601@holbertonschool.com>
2020-03-11 22:21:09 +0000

    modified condition

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (cmp != NULL && array != NULL)
-		if (size <= 0)
-			return (-1);
+	if (cmp && array && size)
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))
 				return (i);
 		}
-		return (-1);
+	return (-1);
 }
