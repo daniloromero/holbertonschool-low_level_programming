84070daa4d16229a8c460298136061e5015f5eef
daniloromero <1601@holbertonschool.com>
2020-03-11 03:09:54 +0000

    tested on console

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *  print_number -print an integer
+ * @n: given integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = i * - 1;
+	}
+	if ((i / 10) > 0)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + 48);
+}
