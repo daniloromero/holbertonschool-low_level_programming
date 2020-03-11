295089102dab7962b206df95296e213fd170761e
daniloromero <1601@holbertonschool.com>
2020-03-11 20:54:02 +0000

    added size condition

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,8 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (action != NULL)
-		return;
+	if (action != NULL && size != 0)
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
