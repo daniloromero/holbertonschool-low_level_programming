d0fc4a9b4586d0a319b854fc26907813215b0a88
daniloromero <1601@holbertonschool.com>
2020-03-12 16:54:20 +0000

    betty styled

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,14 +19,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while(ops[i].op[0] != s[0])
+	while (ops[i].op[0] != s[0])
 	{
-		i++;
 		if (ops[i].op == NULL)
-		{
 			printf("Error\n");
 			exit(99);
 		}
+		i++;
 	}
 	return (ops[i].f);
 }
