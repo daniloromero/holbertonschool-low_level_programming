f137eb0f7caf92a2c5d82c29e68a6d4a954361eb
daniloromero <1601@holbertonschool.com>
2020-03-26 01:27:10 +0000

    changed %i for %d i  the printf expression

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 
 /**
- * print_listint -  prints all the elements of a listint_t list
+ * print_listint - prints all the elements of a listint_t list
  * @h: pointer to head of the list
  * Return: number of nodes
  */
@@ -13,7 +13,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		printf("%i\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
