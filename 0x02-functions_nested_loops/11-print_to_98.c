63eb44113e33dbff594e92e92f70a5534e6b914a
daniloromero <1601@holbertonschool.com>
2020-04-06 00:26:31 +0000

    betty styled

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,13 @@
  */
 void print_to_98(int n)
 {
-	while (n !=98)
+	while (n != 98)
 	{
 		printf("%i, ", n);
-		if (n >98)
-			n = n -1;
+		if (n > 98)
+			n = n - 1;
 		if (n < 98 )
-			n = n +1 ;
+			n = n + 1 ;
 	}
 	printf("%d\n", n);
 }
