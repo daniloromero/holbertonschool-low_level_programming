9e5be9d50b463c7669c9b6a2948c76aa4a49b41b
daniloromero <1601@holbertonschool.com>
2020-04-06 04:49:14 +0000

    removed define

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -2,14 +2,13 @@ section .data
 
 	message db "Hello, Holberton!", 10
 
-section .text
-
+	section .text
 	global _start
 _start:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, message
-	mov rdx, 17 
+	mov rdx, 17
 	syscall
 	mov rax, 60
 	mov rdi, 0
