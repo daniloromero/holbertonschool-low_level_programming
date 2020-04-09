facfd9c6bf008a0ad117b5e66d5e5b319a22ea37
daniloromero <1601@holbertonschool.com>
2020-04-09 21:01:25 +0000

    file creation

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -  generates random valid passwords for the program 101-crackme
+ * 
+ * Return: 0
+ * */
+
+int main(void)
+{
+
+}
