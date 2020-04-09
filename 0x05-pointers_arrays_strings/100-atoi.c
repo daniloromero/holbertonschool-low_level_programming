b1c59ae355756fb879fcaf8cc6abcd19be0ae5a8
daniloromero <1601@holbertonschool.com>
2020-04-09 20:48:10 +0000

    added stdlib and () in conditions

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * _atoi - convert a string to an integer
  * @s: string to be converted
@@ -10,13 +11,13 @@ int _atoi(char *s)
 {
 	int i = 0, neg = 1, integer = 0;
 
-	while (s[i] < '0' || s[i] > '9' && s[i] != NULL)
+	while ((s[i] < '0' || s[i] > '9') && s[i] != NULL)
 	{
 		if (s[i] == '-')
 			neg = neg * -1;
 		i++;
 	}
-	while (s[i] >= '0' || s[i] <= '9' && s[i] != NULL)
+	while ((s[i] >= '0' || s[i] <= '9') && s[i] != NULL)
 	{
 		if (integer >= 0)
 			integer = integer * 10 + (s[i] - '0');
