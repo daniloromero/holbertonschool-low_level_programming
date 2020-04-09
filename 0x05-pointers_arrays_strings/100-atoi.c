e6d082eeecb187810b67aabd36a92af47f40c1cc
daniloromero <1601@holbertonschool.com>
2020-04-09 20:45:44 +0000

    added contition t break loop

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,13 @@ int _atoi(char *s)
 {
 	int i = 0, neg = 1, integer = 0;
 
-	while (s[i] < '0' || s[i] > '9')
+	while (s[i] < '0' || s[i] > '9' && s[i] != NULL)
 	{
 		if (s[i] == '-')
 			neg = neg * -1;
 		i++;
 	}
-	while (s[i] >= '0' || s[i] <= '9')
+	while (s[i] >= '0' || s[i] <= '9' && s[i] != NULL)
 	{
 		if (integer >= 0)
 			integer = integer * 10 + (s[i] - '0');
