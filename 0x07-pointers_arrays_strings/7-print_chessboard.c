d17a6e5139e1d8f46de238ba1028356bd7e1465b
daniloromero <1601@holbertonschool.com>
2020-04-09 22:04:10 +0000

    1st version

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,34 +1,20 @@
 #include "holberton.h"
 
 /**
- *_print_chessboard -  searches in a string  any of a set of bytes.
- *@a: primary string to scan  substring
- *Return:s
+ *_print_chessboard - prints the chessboard.
+ *@a: ponter to array
+ *Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-	int j = 0;
+	int row, j;
 
-
-	if (!*needle)
-		return (haystack);
-	for (j = 0; *haystack; haystack++)
+	for (row = 0; row < 8; row++)
 	{
-		if (*haystack == *needle)
+		for (col = 0; col < 8; col++)
 		{
-			needle++;
-			j++;
+			_putchar(a[row][col]);
 		}
-		else
-		{
-			(needle = needle - j);
-			j = 0;
-		}
-		if (!*needle)
-		{
-			haystack = haystack - j + 1;
-			return (haystack);
-		}
+		_putchar('\n');
 	}
-	return (haystack);
 }
