cf664cea74856f34dc51b5c348c7d88027e8f171
daniloromero <1601@holbertonschool.com>
2020-04-09 05:00:22 +0000

    changed NULL return value to -1

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, writefd, i = 0;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
