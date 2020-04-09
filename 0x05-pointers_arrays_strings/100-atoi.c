e0a8f61d115fae867afa4a5e8cefb9446eaaf33f
daniloromero <1601@holbertonschool.com>
2020-04-09 20:51:19 +0000

    adjusted conditions with 0 instead of NULL

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,13 +11,13 @@ int _atoi(char *s)
 {
 	int i = 0, neg = 1, integer = 0;
 
-	while ((s[i] < '0' || s[i] > '9') && s[i] != NULL)
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
 		if (s[i] == '-')
 			neg = neg * -1;
 		i++;
 	}
-	while ((s[i] >= '0' || s[i] <= '9') && s[i] != NULL)
+	while ((s[i] >= '0' || s[i] <= '9') && s[i] != 0)
 	{
 		if (integer >= 0)
 			integer = integer * 10 + (s[i] - '0');
