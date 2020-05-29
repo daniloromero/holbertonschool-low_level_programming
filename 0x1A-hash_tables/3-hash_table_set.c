1e4a4eaf280e02e39980b7dfb1722718a1c0495d
daniloromero <1601@holbertonschool.com>
2020-05-29 22:34:16 +0000

    Added comparison for key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,6 +21,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
+	while (ht->array[index] != NULL)
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			free(ht->array[index]);
+			ht->array[index]->value = copy;
+			return (1);
+		}
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (free(copy), 0);
