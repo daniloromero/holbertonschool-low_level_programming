a0fb9e5b3aa166bd97cf3672baaf09d2a8499e6e
daniloromero <1601@holbertonschool.com>
2020-05-29 22:54:57 +0000

    Array set to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	hash_table_t *h_t;
+	unsigned long int i = 0;
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (!hash_table)
+	h_t = malloc(sizeof(hash_table_t));
+	if (!h_t)
 		return (NULL);
-	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_table_t *) * size);
+	h_t->size = size;
+	h_t->array = malloc(sizeof(hash_table_t *) * size);
 
-	if (!hash_table->array)
+	if (!h_t->array)
 		return (NULL);
+	for (; i < size; i++)
+		h_t->array[i] = NULL;
 
-	return (hash_table);
+	return (h_t);
 }
