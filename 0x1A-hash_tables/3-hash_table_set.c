fbfbee4d18d6acfc4a9ffdc13b915b4c3eb0bed4
daniloromero <1601@holbertonschool.com>
2020-05-29 22:58:30 +0000

    Removed value from first vaidation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *copy;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
+	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (0);
 
 	copy = strdup(value);
