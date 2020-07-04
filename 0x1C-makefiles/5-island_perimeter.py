1edf250fd4494bd80c6712c6907a1ff6c17bf8ab
daniloromero <1601@holbertonschool.com>
2020-07-04 04:34:43 +0000

    1st version Tested on console

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module to claculate the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """method to calculate the perimeter of grid"""
+    perimeter = 2
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x] == 1:
+                    perimeter += 2
+    return perimeter
