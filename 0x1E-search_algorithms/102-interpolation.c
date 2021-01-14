1d0b5c18d57c60ad31197ff18d66bf8790d5ae3e
daniloromero <1601@holbertonschool.com>
2021-01-14 13:35:20 -0500

    Out of range case

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,7 +12,7 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t low = 0;
 	size_t high = size - 1;
 	size_t pos = low + (((double)(high - low) / (array[high]
-			       - array[low])) * (value - array[low]));
+			      - array[low])) * (value - array[low]));
 
 	if (array == NULL || size < 1)
 		return (-1);
