47fcf8fefb4d83e61e230813e9521a2e2781f654
daniloromero <1601@holbertonschool.com>
2021-01-14 16:29:28 -0500

    TEst

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,67 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - search for value in array
+ * @array: pointer to array of integers
+ * @size: size of array
+ * @value: number given to be searched in array
+ * Return: index of value otherwise -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, L = 0;
+	size_t R = size - 1;
+	size_t m;
+
+	if (array == NULL || size < 1)
+		return (-1);
+
+	while (L <= R)
+	{
+		m = (L + R) / 2;
+		i = L;
+		printf("Searching in array: ");
+		for (; i < R; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		if (array[m] < value)
+			L = m + 1;
+		else if (array[m] > value)
+			R = m - 1;
+		else
+			return (m);
+	}
+	return (-1);
+}
+
+/**
+ * exponential_search - search for value in array
+ * @array: pointer to array of integers
+ * @size: size: of array
+ * @value: number given to be searched in array
+ * Return: index of value otherwise -1
+ */
+
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t bound = 1, init;
+	int *binary = &array[0];
+
+	if (array == NULL || size < 1)
+		return (-1);
+
+//	printf("Value checked array[%ld] = [%d]\n", bound, array[bound]);
+
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", bound, array[bound]);
+		init = bound;
+		bound *= 2;
+	}
+	if (bound > size)
+		bound = size;
+	binary = &(array[init]);
+	printf("Value found between indexes [%ld] and [%ld]\n", init, bound);
+	return (binary_search(binary, size - (bound /2), value));
+}
