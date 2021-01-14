5cc4631cdd77102c2e298752d259e8f259137eb0
daniloromero <1601@holbertonschool.com>
2021-01-14 17:54:41 -0500

    TEst

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - search for value in array
+ * @array: pointer to array of integers
+ * @size: size of array
+ * @value: number given to be searched in array
+ * Return: index of value otherwise -1
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t i, L = 0;
+	size_t R = size - 1;
+	size_t m;
+
+	if (array == NULL || size < 1)
+		return (-1);
+
+	while (L <= R)
+	{
+		m = (L + R) / 2;
+		i = L;
+		printf("Searching in array: ");
+		for (; i < R; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		if (array[m] < value)
+			L = m + 1;
+		else if (array[m] > value)
+			R = m - 1;
+		else
+			return (m);
+	}
+	return (-1);
+}
