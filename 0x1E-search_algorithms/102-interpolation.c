d2ac208e75eb4cf03f3897ad344896863cfc824c
daniloromero <1601@holbertonschool.com>
2021-01-14 13:41:32 -0500

    Out of range case

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -24,8 +24,8 @@ int interpolation_search(int *array, size_t size, int value)
 	while (array[high] != array[low] && value >= array[low] &&
 	       value <= array[high])
 	{
-		pos = low + (((double)(high - low) / (array[high]
-			       - array[low])) * (value - array[low]));
+		pos = low + (((double)(high - low) /
+			      (array[high] - array[low])) * (value - array[low]));
 		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 		if (array[pos] < value)
 			low = pos + 1;
