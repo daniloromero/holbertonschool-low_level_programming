1f08a0b574fdaaf69d30ef55c77f799cc528b0b2
daniloromero <1601@holbertonschool.com>
2021-01-15 08:00:25 -0500

    NULL array case

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,7 +14,7 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t high = size - 1;
 	size_t pos;
 
-	if (!array)
+	if (array == NULL || size < 1)
 		return (-1);
 
 	pos = low + (((double)(high - low) / (array[high]
